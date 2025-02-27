74db5cf2530544b905b68ea0878b1a1af104c6e0
MinWu CHO <mauercho@gmail.com>
2025-02-28 01:35:27 +0900

    until 02-28

--- youtube-demo/routes/channels.js
+++ youtube-demo/routes/channels.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
-router.use(express.json()); // http 외 모듈도 쓰겠다.
+const conn = require("../mariadb");
+router.use(express.json());
 
 let db = new Map();
 let id = 1;
@@ -9,29 +10,25 @@ router
   .route("/")
   .get((req, res) => {
     let { userId } = req.body;
-    let channels = [];
-    if (db.size && userId) {
-      db.forEach(function (value, key) {
-        if (value.userId === userId) channels.push(value);
-      });
 
-      if (channels.length) {
-        res.status(200).json(channels);
-      } else {
-        notFoundChannel();
-      }
+    let sql = `SELECT * FROM channels WHERE user_id = ?`;
+    if (userId) {
+      conn.query(sql, userId, function (err, results) {
+        if (results.length) res.status(200).json(results);
+        else notFoundChannel(res);
+      });
     } else {
-      notFoundChannel();
+      res.status(400).end();
     }
   })
   .post((req, res) => {
-    if (req.body.channelTitle) {
-      let channel = req.body;
-      db.set(id++, channel);
-
-      res
-        .status(201)
-        .json({ message: `${db.get(id - 1).channelTitle}채널을 응원합니다!` });
+    const { name, userId } = req.body;
+    if (name && userId) {
+      let values = [name, userId];
+      let sql = "INSERT INTO channels (name, user_id) VALUES (?, ?)";
+      conn.query(sql, values, function (err, results) {
+        res.status(201).json(results);
+      });
     } else {
       res.status(400).json({ message: "요청 값을 제대로 보내주세요." });
     }
@@ -41,13 +38,11 @@ router
   .route("/:id")
   .get((req, res) => {
     let { id } = req.params;
-    id = parseInt(id);
-    let channel = db.get(id);
-    if (channel) {
-      res.status(200).json(db.get(id));
-    } else {
-      notFoundChannel();
-    }
+    let sql = `SELECT * FROM channels WHERE id = ?`;
+    conn.query(sql, id, function (err, results) {
+      if (results.length) res.status(200).json(results);
+      else notFoundChannel(res);
+    });
   })
   .put((req, res) => {
     let { id } = req.params;
@@ -80,7 +75,7 @@ router
     }
   });
 
-function notFoundChannel() {
+function notFoundChannel(res) {
   res.status(404).json({
     message: "채널 정보를 찾을 수 없습니다.",
   });
--- youtube-demo/routes/users.js
+++ youtube-demo/routes/users.js
@@ -1,45 +1,39 @@
 const express = require("express");
 const router = express.Router();
-router.use(express.json()); // http 외 모듈도 쓰겠다.
 
-let db = new Map();
-let id = 1;
-// 로그인
+const conn = require("../mariadb");
+
+router.use(express.json());
+
 router.post("/login", function (req, res) {
-  // userId가 디비에 저장된 회원인지 확인
-  const { userId, password } = req.body;
-  let loginUser = {};
-  db.forEach(function (user, id) {
-    if (user.userId === userId) {
-      loginUser = user;
-    }
-  });
-  if (isExist(loginUser)) {
-    if (loginUser.password === password) {
+  const { email, password } = req.body;
+  let sql = `SELECT * FROM users WHERE email = ?`;
+  conn.query(sql, email, function (err, results) {
+    let loginUser = results[0];
+    if (loginUser && loginUser.password == password) {
       res
         .status(200)
         .json({ msssage: `${loginUser.name}님 로그인 되었습니다.` });
     } else {
-      res.status(400).json({ msssage: `비밀번호가 틀렸습니다.` });
+      res.status(404).json({
+        message: "이메일 또는 비밀번호가 틀렸습니다.",
+      });
     }
-  } else {
-    res.status(404).json({ msssage: `회원 정보가 없습니다.` });
-  }
+  });
 });
 
-// 회원가입
 router.post("/join", function (req, res) {
-  db.set(id++, req.body);
-
-  if (req.body.size !== 0) {
-    const { userId } = req.body;
-    db.set(userId, req.body);
-    res.status(201).json({
-      message: `${db.get(userId).name}님 환영합니다`,
-    });
-  } else {
+  if (req.body == {}) {
     res.status(400).json({
       message: `입력 값을 다시 확인해주세요.`,
+    });
+  } else {
+    const { email, name, password, contact } = req.body;
+    let values = [email, name, password, contact];
+    let sql =
+      "INSERT INTO users (email, name, password, contact) VALUES (?, ?, ?, ?)";
+    conn.query(sql, values, function (err, results) {
+      res.status(201).json(results);
     });
   }
 });
@@ -47,41 +41,24 @@ router.post("/join", function (req, res) {
 router
   .route("/users")
   .get(function (req, res) {
-    let { userId } = req.body;
-    const user = db.get(userId);
-    if (user) {
-      res.status(200).json({
-        userId: user.userId,
-        name: user.name,
-      });
-    } else {
-      res.status(404).json({
-        message: "회원 정보가 없습니다.",
-      });
-    }
+    let { email } = req.body;
+    let sql = `SELECT * FROM users WHERE email = ?`;
+    conn.query(sql, email, function (err, results) {
+      if (results.length) {
+        res.status(200).json(results);
+      } else {
+        res.status(404).json({
+          message: "회원 정보가 없습니다.",
+        });
+      }
+    });
   })
   .delete(function (req, res) {
-    let { userId } = req.body;
-    const user = db.get(userId);
-    if (user) {
-      db.delete(id);
-      res.status(200).json({
-        message: `${user.name}님 다음에 또 뵙겠습니다.`,
-      });
-    } else {
-      res.status(404).json({
-        message: "회원 정보가 없습니다.",
-      });
-    }
+    let { email } = req.body;
+    let sql = `DELETE FROM users WHERE email = ?`;
+    conn.query(sql, email, function (err, results) {
+      res.status(200).json(results);
+    });
   });
-function isExist(obj) {
-  if (obj.constructor === Object) {
-    if (Object.keys(obj).length === 0) {
-      return false;
-    } else {
-      return true;
-    }
-  }
-}
 
 module.exports = router;
